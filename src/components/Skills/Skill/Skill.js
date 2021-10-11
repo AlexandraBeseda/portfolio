8c34529b4d12318d070cbda2760ff00892e2950d
AlexandraBeseda <s.ani.aa862@gmail.com>
2021-10-11 15:48:46 +0300

    without normal text

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -2,15 +2,25 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import s from "./Skill.module.css";
 
-export const Skill = (props) => {
+export const SkillSquare = (props) => {
     return (
         <div className={s.skill}>
             <h4>{props.title}</h4>
             <div className={s.imageContainer}>
-                <FontAwesomeIcon className={s.icon} icon={props.icon}/>
+                <FontAwesomeIcon className={s.iconSquare} icon={props.icon}/>
             </div>
             <span className={s.description}>{props.description}</span>
         </div>
     );
 }
-
+export const SkillCircle = (props) => {
+    return (
+        <div className={s.skill}>
+            <h4>{props.title}</h4>
+            <div className={s.imageContainer}>
+                <FontAwesomeIcon className={s.iconCircle} icon={props.icon}/>
+            </div>
+            <span className={s.description}>{props.description}</span>
+        </div>
+    );
+}
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,7 +3,7 @@ import s from "./Skills.module.css";
 import styleContainer from "./../../common/styles/Container.module.css"
 import {Title} from "../../common/components/Title";
 import { faCss3Alt,faHtml5, faJs, faNpm, faReact, faYarn} from "@fortawesome/free-brands-svg-icons";
-import {Skill} from "./Skill/Skill";
+import {SkillSquare,SkillCircle} from "./Skill/Skill";
 import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
 
 export const Skills = () => {
@@ -12,13 +12,13 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <Title title={"Skills"}/>
                 <div className={s.skills}>
-                    <Skill title={"JS"} description={"will have to make sure "} icon={faJs}/>
-                    <Skill title={"CSS"} description={"will have to make sure "} icon={faCss3Alt}/>
-                    <Skill title={"HTML"} description={"will have to make sure "} icon={faHtml5}/>
-                    <Skill title={"React"} description={"will have to make sure "} icon={faReact}/>
-                    <Skill title={"NPM"} description={"will have to make sure "} icon={faNpm}/>
-                    <Skill title={"YARN"} description={"will have to make sure "} icon={faYarn}/>
-                    <Skill title={"GITHUB"} description={"will have to make sure "} icon={faGithub}/>
+                    <SkillSquare title={"JS"} description={"will have to make sure "} icon={faJs}/>
+                    <SkillSquare title={"CSS"} description={"will have to make sure "} icon={faCss3Alt}/>
+                    <SkillSquare title={"HTML"} description={"will have to make sure "} icon={faHtml5}/>
+                    <SkillSquare title={"React"} description={"will have to make sure "} icon={faReact}/>
+                    <SkillSquare title={"NPM"} description={"will have to make sure "} icon={faNpm}/>
+                    <SkillCircle title={"YARN"} description={"will have to make sure "} icon={faYarn}  />
+                    <SkillCircle title={"GITHUB"} description={"will have to make sure "} icon={faGithub}/>
 
                 </div>
             </div>
